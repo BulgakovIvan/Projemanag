a4f17c5d452aabd8dd51ec787eafddb8ef77bb7a
BulgakovIvan <bulgivan@mail.ru>
2021-06-25 15:56:41 +0300

    SplashActivity

--- app/src/main/java/com/example/projemanag/SplashActivity.kt
+++ app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.projemanag
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import android.widget.TextView
 
@@ -18,5 +20,10 @@ class SplashActivity : AppCompatActivity() {
 
         val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         findViewById<TextView>(R.id.tv_app_name).typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
