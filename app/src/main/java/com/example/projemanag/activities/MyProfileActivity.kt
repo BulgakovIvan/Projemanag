2c5026391f8a09c86e60e460d33a1c1536a4c392
BulgakovIvan <bulgivan@mail.ru>
2021-07-08 18:24:51 +0300

    start MyProfileActivity

--- app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
@@ -3,10 +3,21 @@ package com.example.projemanag.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivityMyProfileBinding
 
 class MyProfileActivity : BaseActivity() {
+    private lateinit var binding: ActivityMyProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_profile)
+        binding = ActivityMyProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar(binding.toolbarMyProfileActivity)
+        binding.toolbarMyProfileActivity.title = resources.getString(R.string.my_profile_title)
+
+
+
+
     }
 }
