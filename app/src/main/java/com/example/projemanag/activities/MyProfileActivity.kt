b6c23fe6c8ee3a34871446b59b3cb1c806d0078f
BulgakovIvan <bulgivan@mail.ru>
2021-07-09 08:47:44 +0300

    image chooser

--- app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
@@ -1,14 +1,31 @@
 package com.example.projemanag.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivityMyProfileBinding
 import com.example.projemanag.firebase.FirestoreClass
 import com.example.projemanag.models.User
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
     private lateinit var binding: ActivityMyProfileBinding
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +36,70 @@ class MyProfileActivity : BaseActivity() {
         binding.toolbarMyProfileActivity.title = resources.getString(R.string.my_profile_title)
 
         FirestoreClass().loadUserData(this)
+
+        binding.ivProfileUserImage.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "You just denied the permission for storage. You con also allow it from settings.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null) {
+            mSelectedImageFileUri = data.data
+
+            try {
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileUri)
+                    .fitCenter() //.centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(binding.ivProfileUserImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
     }
 
     fun setUserDataInUI(user: User) {
@@ -27,7 +108,7 @@ class MyProfileActivity : BaseActivity() {
             .load(user.image)
             .fitCenter() //.centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(binding.ivUserImage)
+            .into(binding.ivProfileUserImage)
 
         binding.etName.setText(user.name)
         binding.etEmail.setText(user.email)
