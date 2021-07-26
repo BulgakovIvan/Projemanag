7c4130b6220fce4b0f534246bceea6348a842342
BulgakovIvan <bulgivan@mail.ru>
2021-07-26 12:10:49 +0300

    CardDetailsActivity : fun getIntentData()

--- app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
@@ -1,12 +1,36 @@
 package com.example.projemanag.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivityCardDetailsBinding
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
 
-class CardDetailsActivity : AppCompatActivity() {
+class CardDetailsActivity : BaseActivity() {
+    private lateinit var binding: ActivityCardDetailsBinding
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_details)
+        binding = ActivityCardDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getIntentData()
+        setupActionBar(binding.toolbarCardDetailsActivity,
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
+
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
